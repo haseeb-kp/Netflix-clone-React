7b7eba9d195c6c20acad6921e94a7ce68420bb6e
haseeb-kp <haseebkph@gmail.com>
2022-12-27 14:42:08 +0530

    first commit

---
+++ src/Constants/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '3f3481913c1ebe3987e1cfd7bf59e37d';
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
